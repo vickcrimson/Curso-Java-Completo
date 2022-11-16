c75fa82d334f6c5201448fa1e41838a31303176d
Crimson <86634589+vickcrimson@users.noreply.github.com>
2022-11-15 22:54:42 -0300

    Inicio da atividade 1 para todos

---
+++ exercises/src/oo/composicão/Item.java
@@ -0,0 +1,13 @@
+package oo.composicão;
+
+public class Item {
+	 String nome;
+	 int quantidade;
+	 double preco; 
+	 
+	 Item(String nome, int quantidade, double preco){
+		 this.nome = nome;
+		 this.quantidade = quantidade;
+		 this.preco = preco;
+	 }
+}
